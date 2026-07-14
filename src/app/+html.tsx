7f66875c60263e4ca5b3f522a7e4038c53920146
solo3li <hamed3alii.3@gmail.com>
2026-07-14 09:29:15 +0000

    feat: enable RTL layout support and define document structure in +html.tsx

---
+++ src/app/+html.tsx
@@ -0,0 +1,16 @@
+import { ScrollViewStyleReset } from 'expo-router/html';
+
+export default function Root({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="ar" dir="rtl">
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <title>FoxShop</title>
+        <ScrollViewStyleReset />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
