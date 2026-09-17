f79d4831c447c415cacf8922c89a8720a048ff02
solo3li <hamed3alii.3@gmail.com>
2026-09-18 01:06:37 +0300

    feat: initialize core Django settings, URL routing, and project dependencies

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -55,6 +55,7 @@ except Exception:
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,6 +131,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -33,3 +33,13 @@ urlpatterns = [
         path('deliveries/', include('apps.deliveries.urls_driver')),
     ])),
 ]
+
+# Static and media files serving fallback
+from django.conf import settings
+from django.urls import re_path
+from django.views.static import serve
+
+urlpatterns += [
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
